d3e4a485cc2f3e93fe4d9538c2ff0befed16370d
David Pratt <dpratt747@gmail.com>
2025-05-02 16:56:32 +0100

    Add do not discover to example spec

--- src/test/scala/example/ExampleSpec.scala
+++ src/test/scala/example/ExampleSpec.scala
@@ -2,10 +2,12 @@ package example
 
 import example.ExampleSpec.*
 import org.scalacheck.Gen
+import org.scalatest.DoNotDiscover
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
+@DoNotDiscover
 final class ExampleSpec extends AnyFunSpec with ScalaCheckPropertyChecks with Matchers {
   describe("Example") {
     it("should generate an apple string") {
